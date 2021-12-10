65eab0723a50d6afa9ca681da915c9ef8d408e80
Alec Missine <alec_missine@yahoo.com>
2021-12-10 11:11:49 -0500

    Sign XDR by agent, sign and submit by user

--- test/account.mjs
+++ test/account.mjs
@@ -1,21 +1,21 @@
 import test from 'ava' // {{{1
-import { Account } from '../v2/lib/account.mjs'
+import { Poke } from '../v2/lib/account.mjs'
 
 test('get maintainer account by public/secret key', async t => { // {{{1
-  let m2 = await new Account(process.env.MAINTAINER_SECRET)
-  const xdr = m2.put('key1', 'value1').toXDR()
+  let m2 = await new Poke(process.env.MAINTAINER_SECRET)
+  const xdr = m2.put('key1', 'value1').toXDR(null)
   console.log(xdr)
-  let txId = await m2.submit(xdr)
+  let txId = await m2.submit({ xdr: xdr })
   console.log(txId)
   txId = await m2.put('key2', 'value2').submit()
   console.log(txId)
 
-  let m1 = await new Account(process.env.MAINTAINER)
+  let m1 = await new Poke(process.env.MAINTAINER)
   console.log(m1.data())
 
-  m2 = await new Account(process.env.MAINTAINER_SECRET)
+  m2 = await new Poke(process.env.MAINTAINER_SECRET)
   await m2.put('key1', null).put('key2', null).submit()
-  m1 = await new Account(process.env.MAINTAINER)
+  m1 = await new Poke(process.env.MAINTAINER)
   console.log(m1.data())
 
   t.assert(m1.account.id == process.env.MAINTAINER
--- test/add-user.mjs
+++ test/add-user.mjs
@@ -1,5 +1,5 @@
 import test from 'ava' // {{{1
-import { Account } from '../v2/lib/account.mjs'
+import { Poke, Txdr } from '../v2/lib/account.mjs'
 import { Keypair } from 'stellar-sdk'
 
 test('add user', async t => { // {{{1
@@ -8,18 +8,21 @@ test('add user', async t => { // {{{1
   const issuerPK = process.env.ASSET_ISSUER
 
   console.log(`- creating ${userPK}...`)
-  let agent = await new Account(process.env.MAINTAINER_SECRET)
-  let txId = await agent
+  let agent = await new Poke(process.env.MAINTAINER_SECRET)
+  let xdr = await agent
   .begin(userPK)
   .createAccount(userPK, '0')
-  .changeTrust(Account.asset, '1000000', userPK)
-  .end(userPK)
-  .submit({ keys: [agent.keys, userKeys] })
+  .changeTrust(Poke.asset, '1000000', userPK)
+  .end(userPK).toXDR()
+  console.log('  - XDR signed by agent')
+
+  let txId = await new Txdr(xdr).submit({ keys: [userKeys] })
+  console.log('  - XDR signed and submitted by user')
   console.log(`- txId ${txId}`)
 
   console.log(`- funding ${userPK}...`)
-  agent = await new Account(process.env.MAINTAINER_SECRET)
-  txId = await agent.payment(userPK, Account.asset, '1000').submit()
+  agent = await new Poke(process.env.MAINTAINER_SECRET)
+  txId = await agent.payment(userPK, Poke.asset, '1000').submit()
   console.log(`- txId ${txId}`)
 
   t.assert(!!txId, `- UNEXPECTED: '${txId}'`)
