ecb768d1b8e449054753baec85cbb2a90ae7bd11
Alec Missine <alec_missine@yahoo.com>
2022-01-05 19:09:55 -0500

    Create Claimable Balance, txId 1ac75a9e8476bdf7b12cc6acf4c5d1a20162ddff575887d8b38aeb9427603c70

--- test/request-favor.mjs
+++ test/request-favor.mjs
@@ -2,7 +2,7 @@ import test from 'ava' // {{{1
 import { Poke, Txdr, encrypt, decrypt } from '../v2/lib/account.mjs'
 import { Favor, FavorRequest, FavorRequestor } from '../v2/lib/favor.mjs'
 import { delay } from '../v2/lib/utils.mjs'
-import { Keypair, } from 'stellar-sdk'
+import { Keypair } from 'stellar-sdk'
 
 test('request favor', async t => { // {{{1
   const users = await Poke.users()
@@ -13,9 +13,15 @@ test('request favor', async t => { // {{{1
     let decrypted = decrypt(data.td1 + data.td2)
     userKeys.push(Keypair.fromSecret(decrypted))
   }
-  await Favor.add(rqst = new FavorRequest(new FavorRequestor(userKeys[0])))
-  await delay(10000)
-  await Favor.remove(rqst)
+  await Favor.add(rqst = new FavorRequest(
+    new FavorRequestor(userKeys[0]),
+    `Favor description goes here.
+    Favor description consists of one or more lines of text.
+    The total length of the text is limited to 2000 characters.`,
+    9000
+  ))
+  //await delay(10000)
+  //await Favor.remove(rqst)
 
   t.assert(users.length > 0, `- UNEXPECTED: users.length ${users.length}`)
 })
