2ceb5a5135eda570081f7286adda3d9ebceb99fe
Alec Missine <alec_missine@yahoo.com>
2022-05-04 10:37:02 -0400

    Add presenter.mjs

--- www/view/impl/show-browser-ip.mjs
+++ www/view/impl/show-browser-ip.mjs
@@ -4,8 +4,9 @@ class ShowBrowserIpView { // {{{1
   constructor () { // {{{2
   }
 
-  show (ip) { // {{{2
-
+  show (ips) { // {{{2
+    console.log(ips)
+    alert(ips.ipv4.ip) // 174.211.177.108
   }
 
   // }}}2
